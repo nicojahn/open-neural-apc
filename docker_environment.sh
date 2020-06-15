52f30209c26c4dbaeb3b8d546d7da13bfc7a7af0
Nico Jahn <dev@nicojahn.com>
2020-06-15 02:49:53 +0200

    Typo in the image name.

--- docker_environment.sh
+++ docker_environment.sh
@@ -4,6 +4,6 @@ docker stop open-neural-apc
 docker rm open-neural-apc
 
 docker run -d --name open-neural-apc -p 8888:8888 \
-	--gpus all -v $(pwd)/:/tf/ tensorflow/tensorflow2.2.0-gpu-jupyter
+	--gpus all -v $(pwd)/:/tf/ tensorflow/tensorflow:2.2.0-gpu-jupyter
 sleep 3
 docker logs open-neural-apc
