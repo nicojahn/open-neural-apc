68a0d4a68c93ab9a19cc79f4c545abecf10fa117
Nico Jahn <dev@nicojahn.com>
2020-07-28 21:09:39 +0200

    Changed tf version 2.2 to 2.3. Fixes a memory leak problem when running NAPC for a while.

--- docker_environment.sh
+++ docker_environment.sh
@@ -6,7 +6,7 @@ docker rm open-neural-apc
 # --privileged=true : is needed for tensorboard profiling. Usually not recommended.
 # -p 6006:6006 : is needed for tensorboard investigation
 docker run -d --name open-neural-apc -p 8888:8888 \
-	--gpus all -v $(pwd)/:/tf/ tensorflow/tensorflow:2.2.0-gpu-jupyter
+	--gpus all -v $(pwd)/:/tf/ tensorflow/tensorflow:2.3.0-gpu-jupyter
 
 sleep 3
 docker logs open-neural-apc
