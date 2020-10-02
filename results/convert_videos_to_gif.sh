80a61de82e96aa8b72ebb1d076c54768c59eaec9
Nico Jahn <dev@nicojahn.com>
2020-10-02 21:50:28 +0200

    Added smaller update on the gif converter due to github filesize restrictions

--- results/convert_videos_to_gif.sh
+++ results/convert_videos_to_gif.sh
@@ -2,4 +2,4 @@
 source_folder="videos"
 file_extension="mp4"
 target_folder="gifs"
-find $source_folder/ -name 'video*.'$file_extension -type f -exec bash -c 'i="$1"; echo $i | sed -e "s/$2/gif/g" -e "s/videos//g" -e "s/video//g" -e "s/\//gifs\//g" | xargs ffmpeg -y -i $i -vf "fps=10,scale=-1:320" -loop 0' _ {} $file_extension \;
+find $source_folder/ -name 'video*.'$file_extension -type f -exec bash -c 'i="$1"; echo $i | sed -e "s/$2/gif/g" -e "s/videos//g" -e "s/video//g" -e "s/\//gifs\//g" | xargs ffmpeg -y -i $i -vf "fps=10,scale=-1:300" -loop 0' _ {} $file_extension \;
